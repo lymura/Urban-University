59079fb5c448795818db55d9a048772eaf7634a9
lymura <86817851+lymura@users.noreply.github.com>
2024-06-06 13:30:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+
+#Задача «Длина слова»
+a = 'vghkyuhfgvchm'
+print(len(a))
+
+#Задача «Суммы и разности».
+first = 5
+second = 10
+sum = first + second
+diff = first - second
+print('сумма =', sum)
+print('разность =', diff)
+
+#Задача «Среднее арифметическое».
+first = 23.5
+second = 56
+third = 1.6
+mean = (first + second + third)/3
+print(mean)
+
+#Задача «Простые строчки».
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string, ',', first_string)
+
+#Задача «Сложная формула».
+a = 3
+b = 4
+c = 5
+f = (a * b) + (a * c)
+print((f**3)/2)
