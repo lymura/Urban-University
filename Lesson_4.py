f2a930c97fab4b76fd629fc355fe163dff594d54
lymura <86817851+lymura@users.noreply.github.com>
2024-06-07 15:22:26 +0700

    Add files via upload

---
+++ Lesson_4.py
@@ -0,0 +1,9 @@
+my_string = input('Введите строку с пробелами: ')
+print(len(my_string))
+print(my_string.lower())   #Выведите строку my_string в верхнем регистре.
+print(my_string.upper())    #Выведите строку my_string в нижнем регистре.
+print(my_string[0])  #Выведите первый символ строки my_string.
+print(my_string[-1])    #Выведите последний символ строки my_string.
+print(my_string.replace(' ', ''))   #Измените строку my_string, удалив все пробелы.
+
+
