069745cbfcd42283169c92c007a08e75cb6a0e22
lymura <86817851+lymura@users.noreply.github.com>
2024-07-01 10:42:00 +0700

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,24 @@
+#Атрибуты и методы обьекта
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+# Создание объектов класса House
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+# Вызов метода go_to
+h1.go_to(5)  # Выведет числа от 1 до 5
+h2.go_to(10) # Выведет "Такого этажа не существует"
+
+
+
