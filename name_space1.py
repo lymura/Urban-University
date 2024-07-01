00deda2eb4466754dffd4ea4227289c0b45a8c28
lymura <86817851+lymura@users.noreply.github.com>
2024-07-01 09:50:56 +0700

    Add files via upload

---
+++ name_space1.py
@@ -0,0 +1,12 @@
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+test_function()
+
+# Вызовите функцию inner_function вне функции test_function
+    inner_function()
+
+#получиkb ошибку, потому что inner_function не доступна
+# в глобальной области видимости
