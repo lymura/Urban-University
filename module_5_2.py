86efbd70b94c9d4af311fc731a23758f52ec9c47
lymura <86817851+lymura@users.noreply.github.com>
2024-07-03 00:29:40 +0700

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,31 @@
+# Специальные методы классов
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+# Создание объектов класса House
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# Вывод информации о зданиях
+print(h1)
+print(h2)
+
+# Вывод количества этажей
+print(len(h1))
+print(len(h2))
