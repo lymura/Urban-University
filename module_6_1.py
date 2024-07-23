ffdd9e6ec72a7f4cc4605db4a5caf85ab0ccca52
lymura <86817851+lymura@users.noreply.github.com>
2024-07-23 21:33:46 +0700

    Update module_6_1.py
    2 вариант

--- module_6_1.py
+++ module_6_1.py
@@ -1,39 +1,35 @@
 class Animal:
+    alive = True
+    fed = False
+
     def __init__(self, name):
-        self.alive = True
-        self.fed = False
         self.name = name
 
+    def eat(self, food):
+        if isinstance(food, Plant) and food.edible:
+            print(f"{self.name} съел {food.name}")
+            Animal.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            Animal.alive = False
+
 class Plant:
+    edible = False
+
     def __init__(self, name):
-        self.edible = False
         self.name = name
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 class Flower(Plant):
     pass
 
 class Fruit(Plant):
-    def __init__(self, name):
-        super().__init__(name)
-        self.edible = True
+    edible = True
 
 # Создание объектов классов и выполнение действий
 a1 = Predator('Волк с Уолл-Стрит')
@@ -41,12 +37,13 @@ a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
-print(a1.name)
-print(p1.name)
+print(a1.name)  # Волк с Уолл-Стрит
+print(p1.name)  # Цветик семицветик
 
-print(a1.alive)
-print(a2.fed)
-a1.eat(p1)
-a2.eat(p2)
-print(a1.alive)
-print(a2.fed)
+print(Animal.alive)  # True
+print(Animal.fed)    # False
+a1.eat(p1)           # Волк с Уолл-Стрит не стал есть Цветик семицветик
+print(Animal.alive)  # False
+a2.eat(p2)           # Хатико съел Заводной апельсин
+print(Animal.fed)    # True
+
