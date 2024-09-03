1cb0f7c684341a1d0b34f47a5f70ec860426de5a
lymura <86817851+lymura@users.noreply.github.com>
2024-09-03 20:02:43 +0700

    Update moduli_8_1.py
    новая версия

--- moduli_8_1.py
+++ moduli_8_1.py
@@ -1,15 +1,12 @@
 def add_everything_up(a, b):
-    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-        # Оба аргумента числа
+    try:
+        # Пытаемся сложить как числа
         return a + b
-    elif isinstance(a, str) and isinstance(b, str):
-        # Оба аргумента строки
-        return a + b
-    else:
-        # Аргументы разных типов или один из них не соответствует типам int, float, str
+    except TypeError:
+        # Если возникает TypeError, возвращаем строковое представление
         return str(a) + str(b)
 
-# Примеры использования функции
-print(add_everything_up(123.456, 'строка'))  # '123.456строка'
-print(add_everything_up('яблоко', 4215))     # 'яблоко4215'
-print(add_everything_up(123.456, 7))         # 130.456
+# Пример использования
+print(add_everything_up(123.456, 'строка'))  # 123.456строка
+print(add_everything_up('яблоко', 4215))    # яблоко4215
+print(add_everything_up(123.456, 7))        # 130.456
