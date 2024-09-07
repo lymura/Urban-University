7a4ba5c3870285bb7b85ba3945a7a11c1c3c1ee2
lymura <86817851+lymura@users.noreply.github.com>
2024-09-07 20:36:27 +0700

    Add files via upload

---
+++ module_10_3.py
@@ -0,0 +1,53 @@
+import threading
+import time
+import random
+import threading
+import time
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            with self.lock:
+                self.balance += amount
+                print(f"Пополнение: {amount}. Баланс: {self.balance}")
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            time.sleep(0.001)
+
+    def take(self):
+        for _ in range(100):
+            try:
+                self.lock.acquire()
+                amount = random.randint(50, 500)
+                print(f"Запрос на {amount}")
+                if amount <= self.balance:
+                    self.balance -= amount
+                    print(f"Снятие: {amount}. Баланс: {self.balance}")
+                else:
+                    print("Запрос отклонён, недостаточно средств")
+            finally:
+                self.lock.release()
+            time.sleep(0.001)
+
+# Создание объекта банка
+bk = Bank()
+
+# Создание потоков для методов deposit и take
+th1 = threading.Thread(target=bk.deposit)
+th2 = threading.Thread(target=bk.take)
+
+# Запуск потоков
+th1.start()
+th2.start()
+
+# Ожидание завершения работы потоков
+th1.join()
+th2.join()
+
+# Вывод итогового баланса
+print(f"Итоговый баланс: {bk.balance}")
