923c45fd82c152b580d096288f8917d626b753fd
lymura <86817851+lymura@users.noreply.github.com>
2024-11-11 16:44:26 +0700

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -2,11 +2,11 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 # Генераторная сборка для разницы длин строк, если их длины не равны
-first_result = (abs(len(first[i]) - len(second[i])) for i in range(min(len(first), len(second))) if len(first[i]) != len(second[i]))
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
 
 # Генераторная сборка для сравнения длин строк в одинаковых позициях
 second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
+second_result = (len(first[i]) > len(second[i]) for i in range(min(len(first), len(second)))
 
-# Примеры вывода
 print(list(first_result))
 print(list(second_result))
